8e88dcd60b1b2337004537a61f85ac0db9e45461
fungizain <49989439+fungizain@users.noreply.github.com>
2020-08-09 22:50:50 +0800

    Add files via upload

--- facenet/__init__.py
+++ facenet/__init__.py
@@ -4,3 +4,4 @@ from .models.inception_resnet_v1 import InceptionResnetV1
 # multi-tasks cnn model
 from .models.mtcnn import MTCNN, PNet, RNet, ONet
 from .models.utils.detect_face import extract_face
+from .models.resnet import build_resnet
