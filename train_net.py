0b8f3f05091d373a6734552d9537aec8eaf52bae
fungizain <49989439+fungizain@users.noreply.github.com>
2020-08-11 18:09:00 +0800

    Add files via upload

--- train_net.py
+++ train_net.py
@@ -1,29 +1,62 @@
-import os
+import torch
+from pathlib import Path
+from torch.utils.data import Dataset, DataLoader
 
-from train_utils import to_device, DeviceDataLoader
+
+def to_device(data, device):
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    def __init__(self, dl):
+        self.dl = dl
+        self.device = self.get_default_device()
+
+    def __iter__(self):
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        return len(self.dl)
+    
+    @staticmethod
+    def get_default_device():
+        if torch.cuda.is_available():
+            return torch.device('cuda')
+        else:
+            return torch.device('cpu')
+
+#-----------------------------------------------------------------------------------
+
+train_txt_path = Path("./dataset/afg-pt")
+
+class TensorFolder(Dataset):
+    def __init__(self, pt_dir):
+        super(TensorFolder, self).__init__()
+        
+        classes = [d.name for d in pt_dir.iterdir() if d.is_dir()]
+        classes.sort()
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+
+        self.classes = classes
+        self.class_to_idx = class_to_idx
+        self.pts = list(pt_dir.glob('*/*.pt'))
+
+    def __getitem__(self, index):
+        pt = self.pts[index]
+        ft = torch.load(pt)
+        label = self.class_to_idx[pt.parent.stem]
+
+        return ft, label
+
+    def __len__(self):
+        return len(self.pts)
 
 if __name__ == "__main__":
-    batch_size = 250
-    data_dir = Path("./dataset/afg")
+    batch_size = 256
+    data_dir = Path("./dataset/afg-pt")
 
-    stats = ([0.5553, 0.4449, 0.3953], [0.2345, 0.2058, 0.1853])
-    train_tf = tt.Compose([tt.Resize(160),
-                           tt.RandomCrop(32, padding=4, padding_mode='reflect'),
-                           tt.RandomHorizontalFlip(),
-                           tt.ToTensor(),
-                           tt.Normalize(*stats, inplace=True)])
-    valid_tf = tt.Compose([tt.Resize(160),
-                           tt.ToTensor(),
-                           tt.Normalize(*stats)])
-    
-    train_ds = ImageFolder(data_dir.joinpath('train/'), transform=train_tf)
-    valid_ds = ImageFolder(data_dir.joinpath('val/'), transform=valid_tf)
-    
-    train_dl = DataLoader(train_ds, batch_size, shuffle=True, num_workers=0)
+    # train_ds = TensorFolder(data_dir.joinpath('train'))
+    valid_ds = TensorFolder(data_dir.joinpath('val'))
     valid_dl = DataLoader(valid_ds, batch_size*2, num_workers=0)
-    train_dl = DeviceDataLoader(train_dl)
-    valid_dl = DeviceDataLoader(valid_dl)
-    
-    model_name = 'se-resnext50-32x4d'
-
-    build_resnet('se-resnext50-32x4d', 'classic', 2, False)
